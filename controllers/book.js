5cdbd16c5b2655b803437f5e04c686871b4ca88a
S573052 <S573052@nwmissouri.edu>
2024-04-04 20:56:22 -0500

    updated

--- controllers/book.js
+++ controllers/book.js
@@ -1,5 +1,5 @@
 var book = require('../models/book');
-// List of all books
+// List of all book
 // List of all book
 exports.book_list = async function(req, res) {
     try{
@@ -27,12 +27,24 @@ var express = require('express');
 const book_controlers= require('../controllers/book');
 var router = express.Router();
 
+// for a specific Costume.
+exports.book_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await book.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    
 
     
 // for a specific book.
-exports.book_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: book detail: ' + req.params.id);
-};
+// exports.book_detail = function(req, res) {
+// res.send('NOT IMPLEMENTED: book detail: ' + req.params.id);
+// };
 // Handle book create on POST.
 // Handle book create on POST.
 exports.book_create_post = async function(req, res) {
@@ -61,6 +73,26 @@ exports.book_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: book delete DELETE ' + req.params.id);
 };
 // Handle book update form on PUT.
-exports.book_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: book update PUT' + req.params.id);
+// exports.book_update_put = function(req, res) {
+// res.send('NOT IMPLEMENTED: book update PUT' + req.params.id);
+// };
+//Handle Costume update form on PUT.
+exports.book_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await book.findById( req.params.id)
+// Do updates of properties
+if(req.body.book_title)
+toUpdate.book_title = req.body.book_title;
+if(req.body.book_author) toUpdate.book_author = req.body.book_author;
+if(req.body.book_rating) toUpdate.book_rating = req.body.book_rating;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
