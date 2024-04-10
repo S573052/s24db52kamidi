475a60308deda9c937c80fbaf69777a6d61790d8
S573052 <S573052@nwmissouri.edu>
2024-04-10 12:00:27 -0500

    updated

--- routes/book.js
+++ routes/book.js
@@ -7,6 +7,8 @@ router.get('/', book_controlers.book_view_all_Page );
 router.get('/detail', book_controlers.book_view_one_Page);
 router.get('/create', book_controlers.book_create_Page);
 router.get('/update', book_controlers.book_update_Page);
+router.get('/delete', book_controlers.book_delete_Page);
+
 module.exports = router;
 
 
