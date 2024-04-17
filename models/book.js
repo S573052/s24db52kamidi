e58ac489e537dca24bf9642c4f6ae9d2fd69f8c4
S573052 <S573052@nwmissouri.edu>
2024-04-17 17:09:24 -0500

    updated

--- models/book.js
+++ models/book.js
@@ -1,8 +1,21 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
 const bookSchema = mongoose.Schema({
-book_title: String,
-book_author: String,
-book_rating: Number
-})
-module.exports = mongoose.model("book",
-bookSchema)
+  book_title: {
+    type: String,
+    required: true, // Ensure the field is required
+    minlength: [3, 'Book title must be at least 3 characters'],
+    maxlength: [25, 'Book title must be at most 25 characters'],
+    trim: true // Trim whitespace from the input
+  },
+  book_author: {
+    type: String,
+    required: true, // Ensure the field is required
+    minlength: [6, 'Book title must be at least 5 characters'],
+    maxlength: [20, 'Book title must be at most 20 characters'],
+    trim: true // Trim whitespace from the input
+  },
+  book_rating: Number
+});
+
+module.exports = mongoose.model("book", bookSchema);
--- routes/book.js
+++ routes/book.js
@@ -1,12 +1,21 @@
 var express = require('express');
 const book_controlers= require('../controllers/book');
+// A little function to check if we have an authorized user and continue on
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
 var router = express.Router();
+
 /* GET costumes */
 router.get('/', book_controlers.book_view_all_Page );
 
 router.get('/detail', book_controlers.book_view_one_Page);
 router.get('/create', book_controlers.book_create_Page);
-router.get('/update', book_controlers.book_update_Page);
+router.get('/update',secured, book_controlers.book_update_Page);
 router.get('/delete', book_controlers.book_delete_Page);
 
 module.exports = router;
